658dbb79de0f65220d581a078a4ea5cbce8dbb4e
Oscar Varela <zipper91191@gmail.com>
2024-01-05 16:33:23 -0600

    sknds

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,15 @@ import './App.css'
 function App() {
 
   function handleSuccess(response:any) {
+    if (response.status === 'connected') {
+        //logout
+         window.FB.logout(function(response:any) {
+          // user is now logged out
+          console.log(response);
+        }
+      );
+      return;
+    }
     console.log(response.status);
   }
 
