9cba8e10e7918a260bece89c0568265dd2f7fc25
Oscar Varela <zipper91191@gmail.com>
2024-01-11 23:21:54 -0600

    btn

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,11 @@ import './App.css'
 
 function App() {
 
-  function handleSuccess(response:any) {
-
-  return  console.log(response);
-  }
-
-  function handleError(error:any) {
-    console.log(error);
-  }
+ 
   return (
     <>
       <div>
-        <FacebookProvider appId="772585208219547">
-          <LoginButton
-            scope="email"
-            onError={handleError}
-            onSuccess={handleSuccess}
-          >
-            Login via Facebook
-          </LoginButton>
-        </FacebookProvider>
+      <button className="btn btn-primary">Primary</button>
       </div>
     </>
   )
